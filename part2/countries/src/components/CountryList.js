30b033d686e1ae67698c819a0cfea0abc836f7df
Filip <filip.eller@aalto.fi>
2022-01-30 21:24:07 +0200

    Complete Data for countries Step2

--- part2/countries/src/components/CountryList.js
+++ part2/countries/src/components/CountryList.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const CountryList = ({countries}) => {
+const CountryList = ({ countries, setQuery }) => {
   return <ul>
     {countries.map(country =>
-      <li key={country.name.common}>{country.name.common}</li>
+      <li key={country.name.common}>
+        {country.name.common}
+        <button onClick={() => setQuery(country.name.common)}>Show</button>
+      </li>
     )}
   </ul>;
 };
--- part2/countries/src/components/DisplayCountries.js
+++ part2/countries/src/components/DisplayCountries.js
@@ -1,12 +1,12 @@
 import DisplayCountry from "./DisplayCountry"
 import CountryList from "./CountryList"
 
-const DisplayCountries = ({ countries }) => {
+const DisplayCountries = ({ countries, setQuery }) => {
   const display = () => {
     if (countries.length > 10) {
       return <p>Too many matches, please specify.</p>
     } else if (countries.length > 1) {
-      return <CountryList countries={countries}/>
+      return <CountryList countries={countries} setQuery={setQuery}/>
     } else if (countries.length === 1) {
       return <DisplayCountry country={countries[0]} />
     } else {
