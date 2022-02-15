3347f90d57c39d85eb110ef702a22e52b3ae94b7
Filip <filip.eller@aalto.fi>
2022-02-15 21:35:10 +0200

    something wrong with git

--- README.md
+++
@@ -1,2 +0,0 @@
-App deployed to Heroku:
-https://protected-river-17782.herokuapp.com/
--- part2/courseinfo/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = ({ course }) => {
-  return <header>
-    <h2>{course}</h2>
-  </header>;
-};
-
-export default Header
