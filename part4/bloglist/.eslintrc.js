11db3c70ea66452915f690e4481dd642c29c2d45
Filip <filip.eller@aalto.fi>
2022-02-26 23:20:29 +0200

    Initialize blog list, complete step 1.

---
+++ part4/bloglist/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-style': 0,
+    'no-console': 0,
+    'no-unused-vars': 0,
+    'arrow-parens': 0,
+  },
+  ignorePatterns: [
+  ],
+};
