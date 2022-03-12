2abeeca7ffb94283e81577bb59791a1258e2b761
Filip <filip.eller@aalto.fi>
2022-03-12 18:21:20 +0200

    Complete Blog list tests step1

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,11 +1,38 @@
 const mongoose = require('mongoose');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      minLength: 3,
+      required: [true, 'Title is required'],
+    },
+    author: {
+      type: String,
+      minLength: 3,
+      required: [true, 'Author is required'],
+    },
+    url: {
+      type: String,
+      minLength: 3,
+      required: [true, 'URL is required'],
+    },
+    likes: {
+      type: Number,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform: (document, returnedObject) => {
+        /* eslint-disable */
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+      },
+    },
+  }
+);
 
 const Blog = mongoose.model('Blog', blogSchema);
 
