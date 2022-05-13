c052b2068fccebbba35208111b2d20bd2a84914d
Filip <filip.eller@aalto.fi>
2022-05-13 15:41:13 +0300

    Complete blog list tests step 2.

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 describe('Blog', () => {
   beforeEach(() => {
@@ -39,5 +40,16 @@ describe('Blog', () => {
     expect(url).toBeNull()
     const likes = screen.queryByText(/3/)
     expect(likes).toBeNull()
+  })
+
+  test('renders also url and likes after clicking "view"', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText(/view/i)
+    await user.click(button)
+
+    screen.getByText('My Statement on Ukraine')
+    screen.getByText('Barack Obama')
+    screen.getByText(/barackobama.medium.com/)
+    screen.getByText(/3/)
   })
 })
