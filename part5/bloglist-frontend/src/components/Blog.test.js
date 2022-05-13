fa0d83e6e4d832afb26d1cd221191f9205c43c37
Filip <filip.eller@aalto.fi>
2022-05-13 15:46:41 +0300

    Complete blog list tests step 3.

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -5,24 +5,25 @@ import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
 
 describe('Blog', () => {
-  beforeEach(() => {
-    const blog = {
-      title: 'My Statement on Ukraine',
-      author: 'Barack Obama',
-      url: 'https://barackobama.medium.com/my-statement-on-ukraine-dc18ef76ad88',
-      likes: 3,
-      user: {
-        id: '123',
-        username: 'obama',
-      },
-    }
-    const user = {
+  const blog = {
+    title: 'My Statement on Ukraine',
+    author: 'Barack Obama',
+    url: 'https://barackobama.medium.com/my-statement-on-ukraine-dc18ef76ad88',
+    likes: 3,
+    user: {
       id: '123',
       username: 'obama',
-    }
-    const likeBlog = jest.fn()
-    const deleteBlog = jest.fn()
+    },
+  }
+  const user = {
+    id: '123',
+    username: 'obama',
+  }
 
+  const deleteBlog = jest.fn()
+  const likeBlog = jest.fn()
+
+  beforeEach(() => {
     render(
       <Blog
         blog={blog}
@@ -44,12 +45,23 @@ describe('Blog', () => {
 
   test('renders also url and likes after clicking "view"', async () => {
     const user = userEvent.setup()
-    const button = screen.getByText(/view/i)
-    await user.click(button)
+    const view = screen.getByText(/^view$/i)
+    await user.click(view)
 
     screen.getByText('My Statement on Ukraine')
     screen.getByText('Barack Obama')
     screen.getByText(/barackobama.medium.com/)
     screen.getByText(/3/)
+  })
+
+  test('calls likeBlog twice when "like" is clicked twice', async () => {
+    const user = userEvent.setup()
+    const view = screen.getByText(/^view$/i)
+    await user.click(view)
+    const like = screen.getByText(/^like$/i)
+    await user.click(like)
+    await user.click(like)
+
+    expect(likeBlog.mock.calls).toHaveLength(2)
   })
 })
