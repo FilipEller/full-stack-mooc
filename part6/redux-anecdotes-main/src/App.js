f71365f10bad73a17effdf622401b59d04b5a692
Filip <filip.eller@aalto.fi>
2022-05-22 11:46:55 +0300

    Complete anecdotes steps 1 and 2.

--- part6/redux-anecdotes-main/src/App.js
+++ part6/redux-anecdotes-main/src/App.js
@@ -4,28 +4,46 @@ const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
-  const vote = (id) => {
-    console.log('vote', id)
+  const vote = id => {
+    return {
+      type: 'VOTE',
+      data: { id },
+    }
+  }
+
+  const createAnecdote = content => {
+    return {
+      type: 'NEW_ANECDOTE',
+      data: { content },
+    }
+  }
+
+  const addAnecdote = event => {
+    console.log('adding')
+    event.preventDefault()
+    const content = event.target.content.value
+    event.target.content.value = ''
+    dispatch(createAnecdote(content))
   }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map(anecdote => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
           </div>
         </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      ))}
+      <h2>Create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name='content' />
+        </div>
+        <button type='submit'>Create</button>
       </form>
     </div>
   )
