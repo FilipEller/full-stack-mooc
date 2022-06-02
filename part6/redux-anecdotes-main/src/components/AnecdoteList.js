6740f5d35bd648e48acf352855943a8073b4f146
Filip <filip.eller@aalto.fi>
2022-06-02 10:45:45 +0300

    Fix state immutability on anecdote sorting.

--- part6/redux-anecdotes-main/src/components/AnecdoteList.js
+++ part6/redux-anecdotes-main/src/components/AnecdoteList.js
@@ -3,21 +3,21 @@ import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state =>
+    [...state.anecdotes].sort((a, b) => b.votes - a.votes)
+  )
   const dispatch = useDispatch()
   return (
     <div>
-      {anecdotes
-        .sort((a, b) => b.votes - a.votes)
-        .map(anecdote => (
-          <div key={anecdote.id}>
-            <div>{anecdote.content}</div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
-            </div>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   )
 }
