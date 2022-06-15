00149bc56fb648629a84cb1ea7395a89c313257e
Filip <filip.eller@aalto.fi>
2022-06-15 13:38:10 +0300

    Init library-frontend.

---
+++ part8/library-frontend/.eslintrc.js
@@ -0,0 +1,35 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+    'cypress/globals': true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest', 'cypress', 'react-hooks'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    eqeqeq: 'error',
+    'no-console': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
+    'object-curly-bracing': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['build', 'node_modules'],
+}
