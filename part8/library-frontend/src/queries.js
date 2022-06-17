2c0c4d6fbcf55179a71fe219b366eb7189141f72
Filip <filip.eller@aalto.fi>
2022-06-17 13:55:11 +0300

    Complete 8.17, Listing books.

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -14,7 +14,9 @@ export const ALL_BOOKS = gql`
   query {
     allBooks {
       title
-      author
+      author {
+        name
+      }
       published
       id
     }
@@ -35,7 +37,9 @@ export const CREATE_BOOK = gql`
       genres: $genres
     ) {
       title
-      author
+      author {
+        name
+      }
       published
       id
     }
