81fd87d879e3d15e938b0552d39b1c539945e533
Filip <filip.eller@aalto.fi>
2022-06-23 19:32:54 +0300

    Complete 9.7, WebExercises.

--- part9/body-mass-index/index.ts
+++ part9/body-mass-index/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import calculateBmi from './utils/calculateBmi';
+import calculateExercises from './utils/calculateExercises';
+
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -18,6 +21,35 @@ app.get('/bmi', (req, res) => {
 
   const bmi = calculateBmi(height, weight);
   return res.json({ height, weight, bmi });
+});
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { target, daily_exercises } = req.body;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  if (!target || !daily_exercises) {
+    return res.status(400).json({ error: 'parameters missing' });
+  }
+
+  if (isNaN(Number(target))) {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  if (!daily_exercises.length) {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+
+  for (const hours of daily_exercises) {
+    if (isNaN(Number(hours))) {
+      return res.status(400).json({ error: 'malformatted parameters' });
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = calculateExercises(target, daily_exercises);
+  return res.json(result);
 });
 
 const PORT = 3003;
