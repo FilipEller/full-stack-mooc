08e5a3de06cb04895463c10234777e65c02ba122
Filip <filip.eller@aalto.fi>
2022-06-23 13:29:32 +0300

    Start working on 9.3.

--- part9/body-mass-index/bmiCalculator.ts
+++ part9/body-mass-index/bmiCalculator.ts
@@ -1,3 +1,20 @@
+interface Data {
+  height: number;
+  weight: number;
+}
+
+const parseInput = (args: Array<string>): Data => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+  if (isNaN(Number(args[2])) || isNaN(Number(args[3])))
+    throw new Error('Invalid input. Arguments must be numbers.');
+
+  return {
+    height: Number(args[2]),
+    weight: Number(args[3]),
+  };
+};
+
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / (height / 100) ** 2;
   switch (true) {
@@ -10,4 +27,14 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 };
 
-console.log(calculateBmi(180, 74));
+const main = () => {
+  try {
+    const { height, weight } = parseInput(process.argv);
+    console.log(calculateBmi(height, weight));
+  } catch (error: unknown) {
+    console.log('Execution failed.');
+    if (error instanceof Error) {
+      console.log('Error:', error.message);
+    }
+  }
+};
